6ff5bf676a4be13af838d717bfcd6031cadfcbd6
Kiptoo Rugut <mccrug97@gmail.com>
2022-02-06 12:31:52 +0300

    it will define logic in this class

--- src/app/quote.ts
+++ src/app/quote.ts
@@ -1,2 +1,9 @@
 export class Quote {
-}
+    showDescription:boolean;
+    constructor(public id:number,public name:string,public theQuote:string,public author:string,public creationDate:Date,public upvotes:number,public downvotes:number){
+      this.showDescription=false;
+    }
+    // id!:number;
+    // name!:String;
+    // description!:string;
+  }
