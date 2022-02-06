5f1f66d459aa9e35b57a9de50c5ec932383e41ef
Kiptoo Rugut <mccrug97@gmail.com>
2022-02-06 22:31:13 +0300

    it will modify changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,28 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms'
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { QuotesComponent } from './quotes/quotes.component';
+import { QuoteComponent } from './quotes/quotes.component';
 import { TimeCountPipe } from './time-count.pipe';
-import { HighlightDirective } from './highlight.directive';
+import { HighlighterDirective } from './highlight.directive';
+import { QuotesFormComponent } from './quotes-form/quotes-form.component';
+import { QuotesDetailComponent } from './quote-detail/quote-detail.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    QuotesComponent,
+    QuoteComponent,
+    QuotesFormComponent,
+    QuotesDetailComponent,  
     TimeCountPipe,
-    HighlightDirective
+    HighlighterDirective,   
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
