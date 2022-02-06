02e5b9014256b6d48a811015db38f2ba577c8c08
Kiptoo Rugut <mccrug97@gmail.com>
2022-02-06 15:05:00 +0300

    it will add emitting events

--- src/app/quotes-detail/quotes-detail.component.ts
+++ src/app/quotes-detail/quotes-detail.component.ts
@@ -8,6 +8,19 @@ import { Component, OnInit, Input,Output,EventEmitter } from '@angular/core';
 export class QuotesDetailComponent implements OnInit {
   @Input() quote!:Quote;
   @Output() deleteQuote=new EventEmitter<boolean>();
+
+  removeQuote(removeProject:boolean){
+    this.deleteQuote.emit(removeProject)
+  }
+
+  upvote(){
+    this.quote.upvotes+=1
+  }
+
+  downvote(){
+    this.quote.downvotes+=1
+  }
+
   constructor() { }
 
   ngOnInit(): void {
