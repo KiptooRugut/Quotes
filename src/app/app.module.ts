6a100cf39b025879973af52ec3204c5c0363d7b3
Kiptoo Rugut <mccrug97@gmail.com>
2022-02-06 13:48:04 +0300

    it wil add function that can delete quotes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { QuotesComponent } from './quotes/quotes.component';
+import { TimeCountPipe } from './time-count.pipe';
+import { HighlightDirective } from './highlight.directive';
 
 @NgModule({
   declarations: [
     AppComponent,
-    QuotesComponent
+    QuotesComponent,
+    TimeCountPipe,
+    HighlightDirective
   ],
   imports: [
     BrowserModule,
