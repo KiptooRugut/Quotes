2af25a54e178e9efa920faee4cc8d809171b5fe1
Kiptoo Rugut <mccrug97@gmail.com>
2022-02-06 17:07:07 +0300

    it will pass data from quotes-form to the parent component

--- src/app/quotes-form/quotes-form.component.ts
+++ src/app/quotes-form/quotes-form.component.ts
@@ -7,7 +7,18 @@ import { Quote } from '../quote';
   styleUrls: ['./quotes-form.component.css']
 })
 export class QuotesFormComponent implements OnInit {
+  newQuote=new Quote(0,"","","",new Date(),0,0);
+  @Output() addQuote = new EventEmitter<Quote>();
 
+  submitQuote(){
+    if (this.newQuote.name.length==0 || this.newQuote.author.length==0|| this.newQuote.theQuote.length==0){
+      alert("All fields Required!")
+    }
+    else {
+      this.addQuote.emit(this.newQuote);
+      this.newQuote = new Quote(0,"","","",new Date(),0,0);
+    }   
+  }
   constructor() { }
 
   ngOnInit(): void {
