9f99c9e6e26d96e340f988def415c1227efc655a
Kiptoo Rugut <mccrug97@gmail.com>
2022-02-08 12:19:37 +0300

    it will fix the bugs

--- src/app/quote.ts
+++ src/app/quote.ts
@@ -3,7 +3,4 @@ export class Quote {
     constructor(public id:number,public name:string,public theQuote:string,public author:string,public creationDate:Date,public upvotes:number,public downvotes:number){
       this.showDescription=false;
     }
-    // id!:number;
-    // name!:String;
-    // description!:string;
   }
